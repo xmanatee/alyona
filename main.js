edbf9637b617836445ac80d810437e81348499da
xmanatee <xmanatee.one@gmail.com>
2019-02-12 23:59:11 +0300

    pushed through script

--- main.js
+++ main.js
@@ -3,9 +3,21 @@ function log(name, value) {
     console.log("KPSO| " + name + " = '" + value + "' [" + typeof value + "]");
 }
 
+function parseParams() {
+    const callbackResponse = (document.URL).split("#")[1];
+    const responseParameters = (callbackResponse).split("&");
+    const parameterMap = [];
+    for (let i = 0; i < responseParameters.length; i++) {
+        parameterMap[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+    }
+    return parameterMap;
+}
+
 function getAccessToken() {
     const parsedUrl = new URL(window.location.href);
-    let access_token = parsedUrl.searchParams.get("access_token") || sessionStorage.getItem("access_token");
+    let access_token = parseParams().access_token
+        || parsedUrl.searchParams.get("access_token")
+        || sessionStorage.getItem("access_token");
 
     log("parsedUrl.searchParams.get(\"access_token\")", parsedUrl.searchParams.get("access_token"));
     log("sessionStorage.getItem(\"access_token\")", sessionStorage.getItem("access_token"));
